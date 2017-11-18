9e145d3de214b3d7834f04cf2c58352880c88c4e
nwayyanant <nwayyananthlaing@gmail.com>
2017-11-18 22:17:38 +0800

    Python for Finance Series

---
+++ Python Programming for Finance/Python Programming for Finance Lesson 2.py
@@ -0,0 +1,12 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+df = pd.read_csv('tsla.csv', parse_dates = True, index_col =0)
+df[['High','Low']].plot()
+
+plt.show()
