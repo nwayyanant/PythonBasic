2a0f65c38dd09ee0c4683eae8267b3bdf5aa63e0
nwayyanant <nwayyananthlaing@gmail.com>
2017-11-26 23:28:41 +0800

    - update Linear Regresion tutorial 2 - Linear Regression.py

--- ML with skLearn/tutorial 1 - Intro to ML.py
+++ ML with skLearn/tutorial 1 - Intro to ML.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 from sklearn import datasets
 from sklearn import svm
@@ -6,11 +6,12 @@ from sklearn import svm
 digits = datasets.load_digits()
 clf = svm.SVC(gamma =0.0001, C=100)
 
-print(len(digits.data()))
+print(len(digits.data))
 
 x,y = digits.data[:-1], digits.target[:-1]
+clf.fit(x,y)
 
-print ('Predicition:', clf.predict(digit.data[-6]))
-plt.imgshow(digits.image[-6], cmpa=plt.cm_gray_r,interpolation="nearest")
-plt.show
+print('Prediction:',clf.predict(digits.data[-1].reshape(1,-1)))
+plt.imshow(digits.images[-1], cmap = plt.cm.gray_r, interpolation = "nearest")
+plt.show()
       
